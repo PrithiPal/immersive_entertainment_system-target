7bc3c84c1aace20146c2f6c53d773dbf1b1149a3
Jace Manshadi <jsaadatm@sfu.ca>
2019-11-05 02:05:55 -0800

    adding a way to recieve alot of rgb values from the host at one time

--- Makefile
+++ Makefile
@@ -4,16 +4,19 @@ GAMING_PC_EXECUTABLE = gaming-beaglebone
 
 GAMING_PC_ENTRY_FILE = client-target.c
 GAMING_PC_H_LIBRARIES = multicast.h
-OUTPUTDIR = $(HOME)/cmpt433/public/myApps
+OUTPUTDIR = $(HOME)/cmpt433/public/project
 CROSS_TOOL = arm-linux-gnueabihf-
 CC_C = $(CROSS_TOOL)gcc
 CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror
 
 
 all :
+	mkdir -p $(OUTPUTDIR)
 	cp ../gaming-libraries/$(GAMING_PC_H_LIBRARIES) .
 	$(CC_C) $(CFLAGS) $(GAMING_PC_ENTRY_FILE) -o $(OUTPUTDIR)/$(GAMING_PC_EXECUTABLE)
 	rm $(GAMING_PC_H_LIBRARIES)
+	cp $(GAMING_PC_ENTRY_FILE) $(OUTPUTDIR)/$(GAMING_PC_ENTRY_FILE)
 
 clean :
 	rm -f $(OUTPUTDIR)/$(GAMING_PC_EXECUTABLE)
+	rm $(OUTPUTDIR)/$(GAMING_PC_ENTRY_FILE)
--- client-target.c
+++ client-target.c
@@ -14,6 +14,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdlib.h>				// for srand
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define EPOCH_TIMESTAMP_LENGTH 11
+#define EPOCH_TIMESTAMP_INDEX 1
+
+char * printSubString(char * message, long firstIndex, long endIndex);
+
 
 int main(){
 	int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
@@ -45,17 +56,82 @@ int main(){
 	printf("[client-target] setsockoptResponse: %d\n", setsockoptResponse);
 
 	char * message;
-	message = malloc(sizeof(*message) * PACKET_SIZE);
-	for (int i = 0; i < PACKET_SIZE; i++){
+	message = malloc(sizeof(*message) * UDP_MESSAGE_SIZE);
+	for (int i = 0; i < UDP_MESSAGE_SIZE; i++){
 		message[i] = ' ';
 	}
-	message[PACKET_SIZE-1] = 0;
+	message[UDP_MESSAGE_SIZE-1] = 0;
 
 	unsigned addrlen = sizeof (addr);
-	ssize_t recvResponse = recvfrom(socketDescriptor, message, PACKET_SIZE, 0, (struct sockaddr *) &addr, &addrlen);
 
-	printf("[client-target] recvResponse: %d\n", recvResponse);
-	printf("[client-target] message recieved : \n{%s}\n", message);
+	char * currentStringIndex;
+	currentStringIndex = malloc(sizeof(*currentStringIndex) * 1);
+
+	char * lastStringIndex;
+	lastStringIndex = malloc(sizeof(*lastStringIndex) * 1);
+	int ret = 0;
+	do{
+
+		ssize_t recvResponse = recvfrom(socketDescriptor, message, UDP_MESSAGE_SIZE, 0, (struct sockaddr *) &addr, &addrlen);
+
+		printf("[client-target] recvResponse: %d\n", recvResponse);
+		//printf("[client-target] message recieved : \n{%s}\n", message);
+
+		char * timestamp;
+		timestamp = malloc(sizeof(*timestamp) * EPOCH_TIMESTAMP_LENGTH);
+
+		int beginningIndexOfFirstTimestamp = 0, endingIndexOfFirstTimestamp = 0, beginningIndexOfLastTimestamp = 0, endingIndexOfLastTimestamp = 0;
+
+		beginningIndexOfFirstTimestamp = EPOCH_TIMESTAMP_LENGTH+1;
+
+		for (int i = EPOCH_TIMESTAMP_LENGTH; endingIndexOfLastTimestamp == 0; i++){
+			if (message[i] == '/'){
+				endingIndexOfFirstTimestamp = i;
+				beginningIndexOfLastTimestamp = i + 1;
+			}
+			if (message[i] == ']'){
+				endingIndexOfLastTimestamp = i;
+			}
+		}
+
+
+		char * currentStringIndex = printSubString(message, beginningIndexOfFirstTimestamp,  endingIndexOfFirstTimestamp);
+		char * lastStringIndex = printSubString(message, beginningIndexOfLastTimestamp,  endingIndexOfLastTimestamp);
+
+		printf("timestamp : %s\n", printSubString(message, 0,  30) );
+		printf("[client-target] beginningIndexOfFirstTimestamp: %d\n", beginningIndexOfFirstTimestamp);
+		printf("[client-target] endingIndexOfFirstTimestamp : {%d}\n", endingIndexOfFirstTimestamp);
+		printf("[client-target] beginningIndexOfLastTimestamp: %d\n", beginningIndexOfLastTimestamp);
+		printf("[client-target] endingIndexOfLastTimestamp : {%d}\n", endingIndexOfLastTimestamp);
+		printf("timestamp : %s\n", printSubString(message, EPOCH_TIMESTAMP_INDEX,  EPOCH_TIMESTAMP_LENGTH) );
+		printf("currentStringSize : %s\n", currentStringIndex );
+		printf("totalStringSize : %s\n", lastStringIndex );
+
+		ret = strcmp(currentStringIndex, lastStringIndex);
+
+		printf("{%d}\n", ret);
+
+		//{[1572944379-0065471/6040921]
+
+
+	}while(ret != 0);
+
+
 	return 0;
+
+}
+
+char * printSubString(char * message, long firstIndex, long endIndex){
+	char * substr;
+	substr = malloc(sizeof(*substr) * (endIndex - firstIndex+1));
+
+	//std::cout<<"endIndex - firstIndex : " << endIndex - firstIndex << std::endl;
+	int index = 0;
+	for (long i = firstIndex; index < endIndex - firstIndex; i++){
+		//std::cout<<" index : " << index << std::endl;
+		substr[index++] = message[i];
+	}
+	substr[endIndex - firstIndex]=0;
+	return substr;
 
 }
